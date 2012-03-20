b7ca2e972e167acd653eecaceca5dfd12ad8dd76
balashekarreddy@gmail.com <balashekarreddy@gmail.com@d37ff830-5793-494b-b594-a327211f9549>
2012-03-20 23:10:15 +0000

    To send the emails

---
+++ SourceCode/HomeCare/src/com/homecare/utility/EmailUtility.java
@@ -0,0 +1,49 @@
+package com.homecare.utility;
+
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class EmailUtility {
+	public static void sendEmail(){
+		final String username = "dreddy.lnt@gmail.com";
+		final String password = "ultimatedp";
+ 
+		Properties props = new Properties();
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.smtp.host", "smtp.gmail.com");
+		props.put("mail.smtp.port", "587");
+ 
+		Session session = Session.getInstance(props,
+		  new javax.mail.Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(username, password);
+			}
+		  });
+ 
+		try {
+ 
+			Message message = new MimeMessage(session);
+			message.setFrom(new InternetAddress("dreddy.lnt@gmail.com"));
+			message.setRecipients(Message.RecipientType.TO,
+				InternetAddress.parse("prasad14_gitam@yahoo.com"));
+			message.setSubject("Testing Subject");
+			message.setText("Dear Mail Crawler,"
+				+ "\n\n No spam to my email, please!");
+ 
+			Transport.send(message);
+ 
+			System.out.println("Email Send");
+ 
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+}
